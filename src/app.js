f0d2e6243477c572076b42b95dc01e80eebadfd4
CBear <chebert222@gmail.com>
2021-05-03 17:21:34 -0700

    created index.html with node.js

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const generatePage = require('./page-template.js');
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+fs.writeFile("index.html", generatePage(name, github), err => {
+    if (err) throw new error(err);
+
+    console.log('Portfolio complete! Check out index.html to see the output!')
+    
+});
+
+
+
+
+
+
+// console.log(name, github);
+// console.log(generatePage(name, github));
+
