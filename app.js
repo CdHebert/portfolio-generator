46a4f7334673e0dfe89877861f499dc282ccb27b
CBear <chebert222@gmail.com>
2021-05-05 15:25:41 -0700

    added validate to questions

--- app.js
+++ app.js
@@ -16,12 +16,28 @@ const promptUser = () => {
         {
             type: 'input',
             name: 'name',
-            message: 'What is your name?'
+            message: 'What is your name? (Required)',
+            validate: nameInput => {
+              if (nameInput) {
+                return true;
+              } else {
+                console.log('Please enter your name!');
+                return false;
+              }
+            }
         },
         {
             type: 'input',
             name: 'github',
-            message: 'Enter your GitHub Username'
+            message: 'Enter your GitHub Username (Required)',
+            validate: githubInput => {
+                if (githubInput) {
+                  return true;
+                } else {
+                  console.log('Please enter your GitHub Username!');
+                  return false;
+                }
+              }
         },
         {
             type: 'input',
@@ -47,12 +63,28 @@ if (!portfolioData.projects) {
         {
             type: 'input',
             name: 'name',
-            message: 'What is the name of your project?'
+            message: 'What is the name of your project? (Required)',
+            validate: projectNameInput => {
+                if (projectNameInput) {
+                  return true;
+                } else {
+                  console.log('Please enter a project name!');
+                  return false;
+                }
+              }
         },
         {
             type: 'input',
             name: 'description',
-            message: 'Provide a description of the project (Required)'
+            message: 'Provide a description of the project (Required)',
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                  return true;
+                } else {
+                  console.log('Please enter a description!');
+                  return false;
+                }
+              }
         },
         {
             type: 'checkbox',
@@ -63,7 +95,15 @@ if (!portfolioData.projects) {
         {
             type: 'input',
             name: 'link',
-            message: 'Enter the GitHub link to your project. (Required)'
+            message: 'Enter the GitHub link to your project. (Required)',
+            validate: linkInput => {
+                if (linkInput) {
+                  return true;
+                } else {
+                  console.log('Please enter a GitHub Link to the project!');
+                  return false;
+                }
+              }
         },
         {
             type: 'confirm',
